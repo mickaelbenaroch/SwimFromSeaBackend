ec562d05771d880262a5835d8242dc54e85675c4
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-01-06 01:23:41 +0200

    cors

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ var cors = require('cors')
 
 // initialize our express app
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 let port = 3000;
 
