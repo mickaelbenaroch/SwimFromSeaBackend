d5a7f0b7ca46d6b79af63ecebe0acbaf07555edc
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-01-06 01:30:45 +0200

    cors3

--- app.js
+++ app.js
@@ -11,13 +11,7 @@ var cors = require('cors')
 
 // initialize our express app
 const app = express();
-app.use(cors());
-app.options("*",function(req,res,next){
-    res.header("Access-Control-Allow-Origin", req.get("Origin")||"*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     //other headers here
-      res.status(200).end();
-  });
+app.use(allowCrossDomain);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
