4327760a03a4897c3347d100e8e81ea747678bdc
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-01-17 13:34:56 +0200

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var recordModule    = module();
 
 //Cors usage
 app.use(function(req, res, next) {
-    var allowedOrigins = ['https://swimfromsea.herokuapp.com', 'http://localhost:8020', 'http://localhost:3000','http://localhost:4200/'];
+    var allowedOrigins = ['https://swimfromsea.herokuapp.com', 'http://localhost:8020', 'http://localhost:3000','http://localhost:4200'];
     var origin = req.headers.origin;
     if(allowedOrigins.indexOf(origin) > -1){
          res.setHeader('Access-Control-Allow-Origin', origin);
